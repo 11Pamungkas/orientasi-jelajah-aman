d880431dee6226e2c65725e495b994a7da671661
Zaki Kafila Pamungkas <zaki.kafila.pamungkas24020@mhs.uingusdur.ac.id>
2026-09-15 15:10:50 +0700

    feat: buat tab navigation Beranda, Riwayat, Pengaturan

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import RiwayatList from "../../../components/RiwayatList";
+import SearchBox from "../../../components/SearchBox";
+import WeatherCard from "../../../components/WeatherCard";
+
+export default function HalamanUtama() {
+  const [kotaAktif, setKotaAktif] = useState("Pekalongan");
+  const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
+
+  useEffect(() => {
+    console.log("Kota aktif berubah menjadi:", kotaAktif);
+  }, [kotaAktif]);
+
+  function handleCari(kota: string) {
+    setKotaAktif(kota);
+    if (!riwayat.includes(kota)) {
+      setRiwayat([...riwayat, kota]);
+    }
+  }
+
+  return (
+    <View style={{ padding: 16, gap: 16, paddingTop: 70, flex: 1 }}>
+      <SearchBox onCari={handleCari} />
+      <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
+      <RiwayatList daftarKota={riwayat} />
+    </View>
+  );
+}
